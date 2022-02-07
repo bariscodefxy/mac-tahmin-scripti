8ea303b1321a89c0be162f694f1ecc695d15f431
bariscodefxy <bariscodefx@gmail.com>
2022-02-07 18:35:58 +0300

    update script again

---
+++ api/canlimaclar.php
@@ -0,0 +1,56 @@
+<?php
+
+if(@$_GET['tarih'] == "") {
+    $uyari = 'Lütfen Tarihi Giriniz.';
+    die($uyari);
+}
+$date=explode("/",$_GET['tarih']);
+if(checkdate ($date[1] ,$date[0] ,$date[2]))
+{
+    $tarih = $_GET['tarih'];
+}
+else 
+{
+    $uyari = 'Lütfen Tarihi Giriniz.';
+    die($uyari);
+}
+$veri = file_get_contents("http://goapi.mackolik.com/livedata?date=" . $_GET['tarih']);
+$dizi = json_decode($veri, true);
+$maclar = $dizi['m'];
+$v = "0";
+$sayi = count($maclar);
+    for ($i=0; $i<$sayi; $i++)
+      {
+        if (empty($maclar[$i][6]) || $maclar[$i][6] === "MS")
+        {
+            unset($maclar[$i]);
+        }
+        else
+        {
+            $mac[$v] = $maclar[$i];
+            $v++;
+        }
+
+      }
+$sayi1 = count($mac);
+echo "<h1 class='display-2 text-center fw-bold'>Canlı Maç Sonuçları</h1>
+<table class='table table-responsive table-dark table-bordered table-hover'>
+  <tr>
+    <th>İddia Kodu</th>
+    <th>Dakika</th> 
+    <th>Ev Sahibi</th>
+    <th>Deplasman</th>
+    <th>Skor</th>
+  </tr>";
+       for ($i=0; $i<$sayi1; $i++)
+      {
+        echo "<tr>";
+        echo "<td>" .$mac[$i][14]. "</td>";
+        echo "<td>" .$mac[$i][6]. "</td>";
+        echo "<td>" .$mac[$i][2]. "</td>";
+        echo "<td>" .$mac[$i][4]. "</td>";
+        echo "<td>" .$mac[$i][12]. " - " .$mac[$i][13]. "</td>";
+        echo "</tr>";
+      }
+echo "</table>";
+    ?>
